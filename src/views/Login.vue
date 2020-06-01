cb49c9a254a40ae965227cc2e9d6c8d8493c4016
ybsdegit <ybsdeyx@163.com>
2020-06-01 23:26:12 +0800

    login token

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,15 +1,136 @@
 <template>
   <div>
-    login
+    <el-container>
+      <el-header>
+        <img class="mlogo" src="../assets/logo.png" />
+      </el-header>
+      <el-main>
+        <el-form
+          :model="ruleForm"
+          :rules="rules"
+          ref="ruleForm"
+          label-width="100px"
+          class="demo-ruleForm"
+        >
+          <el-form-item label="用户名" prop="username">
+            <el-input v-model="ruleForm.username"></el-input>
+          </el-form-item>
+
+          <el-form-item label="密码" prop="password">
+            <el-input type="password" v-model="ruleForm.password"></el-input>
+          </el-form-item>
+
+          <el-form-item>
+            <el-button type="primary" @click="submitForm('ruleForm')"
+              >登录</el-button
+            >
+            <el-button @click="resetForm('ruleForm')">重置</el-button>
+          </el-form-item>
+        </el-form>
+      </el-main>
+    </el-container>
   </div>
 </template>
 
 <script>
 export default {
-  
+  data() {
+    return {
+      ruleForm: {
+        username: 'paulson',
+        password: '111111',
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          {
+            min: 3,
+            max: 10,
+            message: '长度在 3 到 10 个字符',
+            trigger: 'blur',
+          },
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          {
+            min: 3,
+            max: 10,
+            message: '长度在 3 到 10 个字符',
+            trigger: 'blur',
+          },
+        ],
+      },
+    }
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          const _this = this
+          axios
+            .post('http://localhost:8080/login', this.ruleForm)
+            .then((res) => {
+              const token = res.headers['authorization']
+              const userInfo = res.data.data
+              _this.$store.commit('SET_TOKEN', token)
+              _this.$store.commit('SET_USERINFO', userInfo)
+            })
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields()
+    },
+  },
 }
 </script>
 
 <style scoped>
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
 
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  /* background-color: #e9eef3; */
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
+
+.mlogo {
+  height: 80%;
+  margin-top: 8px;
+}
+
+.demo-ruleForm {
+  max-width: 500px;
+  margin: 0 auto;
+}
 </style>
