a1f8a7f688fe4e2679f1b4fd1cbd4e9c8cac3870
ybsdegit <ybsdeyx@163.com>
2020-06-01 22:44:49 +0800

    add vue page

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,38 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
   const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Index',
+    redirect: { name: 'Blogs' }
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/blogs',
+    name: 'Blogs',
+    component: () => import('../views/Blogs.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/blog/add',
+    name: 'BlogEdit',
+    component: () => import('../views/BlogEdit.vue')
+  },
+  {
+    path: '/blog/:blogId',
+    name: 'BlogDetail',
+    component: () => import('../views/BlogDetail.vue')
+  },
+  {
+    path: '/blog/:blogId/edit',
+    name: 'BlogEdit',
+    component: () => import('../views/BlogEdit.vue')
   }
 ]
 
