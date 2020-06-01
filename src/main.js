6fecda4e9349d1c9b4924fcc1eb35484e587085e
ybsdegit <ybsdeyx@163.com>
2020-06-01 22:32:59 +0800

    feat-page

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
 import Vue from 'vue'
+import './plugins/axios'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './plugins/element.js'
+import Element from 'element-ui'
+import "element-ui/lib/theme-chalk/index.css"
+// import axios from 'axios'
+
+Vue.use(Element)
+
 
 Vue.config.productionTip = false
+// Vue.prototype.$axios = axios
 
 new Vue({
   router,
